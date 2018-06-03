9feefb7296bb7629dfe421455656c8951716c2e1
Prashant Acharya <prashant_acharya74@yahoo.com>
2018-06-03 15:06:31 +0530

    made changes to the code for handling unhandled exceptions

--- process_records_from_queue_to_dynamodb.py
+++ process_records_from_queue_to_dynamodb.py
@@ -33,31 +33,37 @@ def process_records(record):
 
 
 def process_mutliple_messages(sqs_queue_name):
-    # Create SQS client
-    sqs = setup_queues_topic.sqs_resource
-    # Receive message from SQS queue
-    queue = sqs.get_queue_by_name(QueueName=sqs_queue_name)
-    while True:
-        messages_to_delete = []
-        for message in queue.receive_messages(
-                     MaxNumberOfMessages=10):
-            body = json.loads(ast.literal_eval(json.loads(message.body)["Message"]))
-            process_records(record=body)
-            # add message to delete
-            messages_to_delete.append({
-            'Id': message.message_id,
-            'ReceiptHandle': message.receipt_handle
-           })
+    try:
+        # Create SQS client
+        sqs = setup_queues_topic.sqs_resource
+        # Receive message from SQS queue
+        queue = sqs.get_queue_by_name(QueueName=sqs_queue_name)
+        while True:
+            messages_to_delete = []
+            for message in queue.receive_messages(
+                         MaxNumberOfMessages=10):
+                body = json.loads(ast.literal_eval(json.loads(message.body)["Message"]))
+                process_records(record=body)
+                # add message to delete
+                messages_to_delete.append({
+                'Id': message.message_id,
+                'ReceiptHandle': message.receipt_handle
+               })
 
-        # if you don't receive any notifications the
-        # messages_to_delete list will be empty
-        if len(messages_to_delete) == 0:
-                time.sleep(10)
-         # delete messages to remove them from SQS queue
-         # handle any errors
-        else:
-             delete_response = queue.delete_messages(
-                     Entries=messages_to_delete)
+            # if you don't receive any notifications the
+            # messages_to_delete list will be empty
+            if len(messages_to_delete) == 0:
+                    time.sleep(10)
+             # delete messages to remove them from SQS queue
+             # handle any errors
+            else:
+                 delete_response = queue.delete_messages(
+                         Entries=messages_to_delete)
+    except ValueError:
+        print("Error while processing the queue")
+    except:
+        print("Error while processing the queue")
+
 
 if __name__ == '__main__':
     process_mutliple_messages(config.QUEUE2)
