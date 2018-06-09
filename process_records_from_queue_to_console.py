8ca92943e00846e150842e3933fe023ac9f69f3d
Prashant Acharya <prashant_acharya74@yahoo.com>
2018-06-09 17:03:35 +0530

    adding logging statements

--- process_records_from_queue_to_console.py
+++ process_records_from_queue_to_console.py
@@ -3,10 +3,17 @@ import time
 import config
 import json
 import setup_queues_topic
+import logging
 
+
+logging.basicConfig(filename='fanout_process.log', 
+                    format='%(asctime)s %(message)s', 
+                    datefmt='%m/%d/%Y %I:%M:%S %p', 
+                    level=logging.INFO)
 
 def process_mutliple_messages(sqs_queue_name):
     # Create SQS client
+    logging.info("Inside the process_mutliple_messages function")
     sqs = setup_queues_topic.sqs_resource
     messages_to_process = []
    # Receive message from SQS queue
@@ -16,7 +23,7 @@ def process_mutliple_messages(sqs_queue_name):
         for message in queue.receive_messages(
                      MaxNumberOfMessages=10):
             body = json.loads(message.body)
-            print('Received message: %s' % body['Message'])
+            logging.debug("extracting the body from the queue: ${}".format(body['Message']))
 
             # add message to delete
             messages_to_delete.append({
@@ -33,7 +40,10 @@ def process_mutliple_messages(sqs_queue_name):
         else:
              delete_response = queue.delete_messages(
                      Entries=messages_to_delete)
+             logging.info("Deleting of queue completed")
+
 
 if __name__ == '__main__':
+    logging.info("Invoking the process to extract the queue : {}".format(config.QUEUE2))
     process_mutliple_messages(config.QUEUE1)
 
