c44b0268b7e70ea90a4b1987b9d05630c3df4c3e
Prashant Acharya <prashant_acharya74@yahoo.com>
2018-06-09 07:42:14 +0530

    added the logging module

--- process_records_from_queue_to_dynamodb.py
+++ process_records_from_queue_to_dynamodb.py
@@ -5,14 +5,19 @@ import botocore
 import config
 import json
 import setup_queues_topic 
+import logging
+
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 
 def process_records(record):
+    logging.info("Inside the process_records function")
     dynamodb = setup_queues_topic.dynamodb_client
 
     security_id=record["SECURITY_ID"]
 
     try:
+        logging.debug("trying to insert into the dynamodb table")
         dynamodb.put_item(TableName=config.DYNAMODBTBL,
                            Item={"Id":{
                                     "S": security_id},
@@ -24,25 +29,29 @@ def process_records(record):
                                   }
                                 },
                            ConditionExpression='attribute_not_exists(Id)')
-        print("Inserted record {}, into dynamodb table successfully.".format(record))
+        logging.debug("Inserted record {}, into dynamodb table successfully.".format(record))
     except botocore.exceptions.ClientError as e:
         # Ignore the ConditionalCheckFailedException, bubble up
         # other exceptions.
         if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            logging.error("Error occurred while inserting into the dynamodb table ${0}".format(e))
             raise
 
 
 def process_mutliple_messages(sqs_queue_name):
+    logging.info("Inside the process_mutliple_messages function")
     try:
         # Create SQS client
         sqs = setup_queues_topic.sqs_resource
         # Receive message from SQS queue
         queue = sqs.get_queue_by_name(QueueName=sqs_queue_name)
+        logging.debug("Getting the message from the queue: ${}".format(queue))
         while True:
             messages_to_delete = []
             for message in queue.receive_messages(
                          MaxNumberOfMessages=10):
                 body = json.loads(ast.literal_eval(json.loads(message.body)["Message"]))
+                logging.debug("extracting the body from the queue: ${}".format(body))
                 process_records(record=body)
                 # add message to delete
                 messages_to_delete.append({
@@ -59,6 +68,7 @@ def process_mutliple_messages(sqs_queue_name):
             else:
                  delete_response = queue.delete_messages(
                          Entries=messages_to_delete)
+                 logging.info("Deleting of queue completed")
     except ValueError:
         print("Error while processing the queue")
     """
@@ -67,5 +77,6 @@ def process_mutliple_messages(sqs_queue_name):
     """
 
 if __name__ == '__main__':
+    logging.info("Invoking the process to extract the queue : {}".format(config.QUEUE2))
     process_mutliple_messages(config.QUEUE2)
-
+    
