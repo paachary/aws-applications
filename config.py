c9d881d558dccc09ae2f144728980e9602df10d8
Prashant Acharya <prashant_acharya74@yahoo.com>
2018-06-09 05:58:10 +0530

    added the aws configure region command to the startup script

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Application for testing the fanout scenario using sns and sqs
 QUEUE1 = "FanOutQueue-1"
 QUEUE2 = "FanOutQueue-2"
-TOPIC = "FanOutTopic"
+TOPIC = "FanOutSNSTopic"
--- start_application.sh
+++ start_application.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+aws configure set region `curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
 cd /home/ec2-user
 python process_records_from_queue_to_dynamodb.py
